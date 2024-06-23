fbf3cebf680a5e4f9cced0f8fab368cd464867de
Nguyen-Hong-Quang <quangdegea13@gmail.com>
2024-06-23 16:52:11 +0700

    add hello

--- tinh tien nuoc.cs
+++ tinh tien nuoc.cs
@@ -24,26 +24,28 @@ namespace ass_nhap
             {
                 Console.WriteLine("Error: Current water meter reading must be greater than or equal to the previous reading. Please enter again.");
                 Console.WriteLine("Current water meter reading:");
+                previousWaterComsuption = float.Parse(Console.ReadLine());
                 currentWaterComsuption = float.Parse(Console.ReadLine());
             }
 
             // Calculate the water usage for this month
             float waterUsed = currentWaterComsuption - previousWaterComsuption;
            
-            // Display the result
+            // Display the result water comsumed
             Console.WriteLine("Water Comsumed: " + waterUsed +"m^3");
 
             // Process billing based on user choice
             ProcessBilling(waterUsed);
         }
 
-        
+        //Enter user name
         static string GetUserName()
         {
             Console.WriteLine("Enter username:");
             return Console.ReadLine();
         }
 
+        //Enter the water meter usage index
         static float GetWaterMeterComsuption(string meterwaterType)
         {
             float meterwater = 0;
@@ -63,11 +65,13 @@ namespace ass_nhap
             return meterwater;
         }
 
+        // Process billing based on user choice
         static void ProcessBilling(float waterUsed)
         {
             string userInput;
             do
             {
+                // Display customer types
                 Console.WriteLine("List of options:\r\n" +
                     "1. Household\r\n" +
                     "2. Administrative Agencies and Public Service \r\n" +
@@ -76,6 +80,7 @@ namespace ass_nhap
                 Console.Write("Types of customer: ");
                 userInput = Console.ReadLine();
 
+                // Calculate water charges based on customer type
                 switch (userInput)
                 {
                     case "1":
@@ -98,7 +103,7 @@ namespace ass_nhap
             Console.ReadKey();
         }
 
-
+        // Calculate water bill for household 
         static void HouseholdBilling(float waterUsed)
         {
             Console.WriteLine("Enter the number of people in your household:");
@@ -123,7 +128,7 @@ namespace ass_nhap
                 }
                 Console.WriteLine("Water bill: " + waterbill + "VND");
 
-
+                // Calcultate fees and total bill  for household 
                 double priceEnviromentFee = (waterbill / 100) * 10;
                 Console.WriteLine("Enviroment Fee : " + priceEnviromentFee + "VND");
 
@@ -139,11 +144,13 @@ namespace ass_nhap
             }
         }
 
+        // Calculate water bill for Administrative Agencies and Public Service
         static void AgencyBilling(float waterUsed, double price)
         {
             double waterbill = waterUsed * price;
             Console.WriteLine("Water bill: " + waterbill + "VND");
 
+            // Calcultate fees and total bill  for Administrative Agencies and Public Service
             double priceEnviromentFee = (waterbill / 100) * 10;
             Console.WriteLine("Enviroment Fee : " + priceEnviromentFee + "VND");
 
@@ -154,11 +161,13 @@ namespace ass_nhap
             Console.WriteLine("Total Bill: " + finalTotalAmount + "VND");
         }
 
+        // Calculate water bill for Production Units
         static void ProductionBilling(float waterUsed, double price)
         {
             double waterbill = waterUsed * price;
             Console.WriteLine("Water bill: " + waterbill + "VND");
 
+            // Calcultate fees and total bill for Production Units
             double priceEnviromentFee = (waterbill / 100) * 10;
             Console.WriteLine("Enviroment Fee : " + priceEnviromentFee + "VND");
 
@@ -170,11 +179,13 @@ namespace ass_nhap
             Console.WriteLine("Total Bill: " + finalTotalAmount + "VND");
         }
 
+        // Calculate water bill for Business Service
         static void BusinessServiceBilling(float waterUsed, double price)
         {
             double waterbill = waterUsed * price;
             Console.WriteLine("Water bill: " + waterbill + "VND");
 
+            // Calcultate fees and total bill for Business Service
             double priceEnviromentFee = (waterbill / 100) * 10;
             Console.WriteLine("Enviroment Fee : " + priceEnviromentFee + "VND");
 
